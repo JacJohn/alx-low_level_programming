893dda3c566f4e63287e50589831bf200bbff51d
JacJohn <jantjiesdenver41@gmail.com>
2022-06-13 09:12:13 -0700

    copies a string src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
